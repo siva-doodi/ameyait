4eec88800c7a1fcd3dce072d864825a41b080b8a
sivadoodi <doodisiva5@gmail.com>
2025-08-11 12:30:41 +0530

    Fix page.js and component imports for GitHub Pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
-import Image from "next/image";
-import HeroSection from './components/home/HeroSection'
-import AmeyaSection from './components/home/AmeyaSection'
-import ServiceCard from './components/home/ServiceCard'
-import Contact from './components/home/contact'
-export default function Home() {
+// File: src/app/home/page.js
+
+import HeroSection from './components/home/HeroSection';
+import AmeyaSection from './components/home/AmeyaSection';
+import ServiceCard from './components/home/ServiceCard';
+import Contact from './components/home/Contact'; // Ensure "Contact" matches exact file name
+
+export default function HomePage() {
   return (
     <>
-
-    <HeroSection />
-    <AmeyaSection />
-    <ServiceCard />
-    <Contact />
+      <HeroSection />
+      <AmeyaSection />
+      <ServiceCard />
+      <Contact />
     </>
   );
 }
